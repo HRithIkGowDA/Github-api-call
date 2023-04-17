7f2dce7ba936d4f7d83e7a4aff046e64ee337584
viru <38450997+HRithIkGowDA@users.noreply.github.com>
2023-04-17 21:24:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserRepositories from './UserRepositories';
+
+function App() {
+  return (
+    <div>
+      <UserRepositories />
+    </div>
+  );
+}
+
+export default App;
